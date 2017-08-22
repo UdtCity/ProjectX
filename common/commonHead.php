758f3181687c6146d18fefb0dd7ee75278e99a37
Rinesh Bharath <rinesh93bharath@hotmail.com>
2017-08-22 14:15:49 +0530

    Add files via upload

---
+++ common/commonHead.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>HOME</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords" content="n" />
+    <link href="assets/css/bootstrap.css" rel='stylesheet' type='text/css' />
+    <link href="assets/css/style.css" rel='stylesheet' type='text/css' />
+    <link href="assets/css/font-awesome.css" rel="stylesheet" type='text/css' >
+    <link href="assets/css/icon-font.min.css" rel="stylesheet" type='text/css' />
+    <script src="assets/js/jquery.js"></script>
+</head>
