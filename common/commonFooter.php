c58236841c5a19c2f8b1675e4a9838f603cb3c33
Bala Kumar <bala02.10.92@Gmail.com>
2017-08-24 01:28:26 +0530

    Add files via upload

--- common/commonFooter.php
+++ common/commonFooter.php
@@ -1,3 +1,43 @@
+<div class="fo-top-di">
+			<div class="foot-top">
+				
+					<div class="col-md-6 s-c">
+						<li>
+							<div class="fooll">
+								<h1>follow us on</h1>
+							</div>
+						</li>
+						<li>
+							<div class="social-ic">
+								<ul>
+									<li><a href="#"><i class="facebok"> </i></a></li>
+									<li><a href="#"><i class="twiter"> </i></a></li>
+									<li><a href="#"><i class="goog"> </i></a></li>
+									<li><a href="#"><i class="be"> </i></a></li>
+										<div class="clearfix"></div>	
+								</ul>
+							</div>
+						</li>
+							<div class="clearfix"> </div>
+					</div>
+					<div class="col-md-6 s-c">
+						<div class="stay">
+									<div class="stay-left">
+										<form>
+											<input type="text" placeholder="Enter your email" required="">
+										</form>
+									</div>
+									<div class="btn-1">
+										<form>
+											<input type="submit" value="join">
+										</form>
+									</div>
+										<div class="clearfix"> </div>
+						</div>
+					</div>
+					<div class="clearfix"> </div>
+				
+			</div>
 <div class="footer">
     <div class="col-md-3 cust">
         <h4>CUSTOMER CARE</h4>
@@ -32,6 +72,7 @@
     <div class="clearfix"> </div>
     <p>© 2016 Gretong. All Rights Reserved | Design by RINA</p>
 </div>
+    </div>
 
 <script src="assets/js/jquery.nicescroll.js"></script>
 <script src="assets/js/bootstrap.js"></script>
